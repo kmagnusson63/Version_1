8ea4eac64ab8d98ac094860b81ecb6c89263f335
Ken Magnusson <acdcvideo@mymts.net>
2015-05-11 12:09:01 -0500

    Reset

--- JavaGameServer/src/javagameserver/JavaGameServer.java
+++ JavaGameServer/src/javagameserver/JavaGameServer.java
@@ -5,6 +5,9 @@
  */
 package javagameserver;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  *
  * @author Dad
@@ -16,6 +19,14 @@ public class JavaGameServer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame();
+        frame.setTitle("My First Swing Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Welcome");
+        frame.add(label);
+        frame.pack();
+        frame.setVisible(true);
     }
     
 }
